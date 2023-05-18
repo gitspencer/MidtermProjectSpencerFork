c7a34403dd8a70738afd505533046f5992fe88e7
Jacob <jwv928@gmail.com>
2023-05-18 13:27:11 -0400

    create Spring Boot MVC project, configure JPA

---
+++ CoastToCoastSkyReport/src/main/java/com/skilldistillery/skyreport/CoastToCoastSkyReportApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.skyreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoastToCoastSkyReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoastToCoastSkyReportApplication.class, args);
+	}
+
+}
